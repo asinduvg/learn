880711c684afb6143b55c1c2573a72d1dbf12404
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-01 23:20:57 +0530

    User-defined Variables and Parameter expansion

---
+++ bash-scripting/2-variables-and-shell-expansions/variable_practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+employee="Sarah"
+echo "Dear ${employee}" # parameter expansion
+echo "The board and I would like to thank you for the excellent work that
+you have been doing over the last few months"
+echo "Keep up the good work, ${employee}"
+
+echo "Best wishes,"
+echo "Karen, CEO"
