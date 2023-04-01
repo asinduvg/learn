ef2c85bba79729e89ed691d31532df7b76779ecb
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-01 21:08:13 +0530

    Bash Script Structure - Part 2 - Professional Components

--- bash-scripting/1-how-to-build-a-bash-script/first_script.sh
+++ bash-scripting/1-how-to-build-a-bash-script/first_script.sh
@@ -2,5 +2,15 @@
 
 # upper line is called shebang line
 
+# Author: Asindu Vidanagamachchi
+# Date Created: 01/04/2023
+# Last Modified: 01/04/2023
+
+# Description
+# Prints "This is my first script" to the termninal
+
+# Usage
+# first_script
+
 echo "This is my first script"
 exit 0 # 0 => successful, 1 - 255 => unsuccessful
