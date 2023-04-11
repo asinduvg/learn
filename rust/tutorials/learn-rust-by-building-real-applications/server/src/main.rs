f3dbe18762286368809bdbf7f76373fa4ddcf8ae
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-11 19:30:28 +0530

    Complete Enum and Option Enum

--- rust/tutorials/learn-rust-by-building-real-applications/server/src/main.rs
+++ rust/tutorials/learn-rust-by-building-real-applications/server/src/main.rs
@@ -1,21 +1,15 @@
 fn main() {
-    // let string = String::from("127.0.0.1:8080");
-    // let string_slice = &string[10..14];
-    // let string_borrow: &str = &string;
-    // let string_literal = "1234";
-
-    // dbg!(&string);
-    // dbg!(string_slice);
-    // dbg!(string_borrow);
-    // dbg!(string_literal);
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let pUT = Method::PUT;
 
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
 struct Server {
-    addr: String,
-
+    addr: String
 }
 
 impl Server {
@@ -28,4 +22,22 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }
