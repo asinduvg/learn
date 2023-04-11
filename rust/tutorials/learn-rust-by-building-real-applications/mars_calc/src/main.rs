431a60c1df8ab36980b97317b1e2570537dd8fac
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-11 09:00:12 +0530

    Complete Finishing Touches

--- rust/tutorials/learn-rust-by-building-real-applications/mars_calc/src/main.rs
+++ rust/tutorials/learn-rust-by-building-real-applications/mars_calc/src/main.rs
@@ -1,32 +1,15 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg): ");
     let mut input = String::new();
     
-    io::stdin().read_line(&mut input);
-
-    borrow_string(&input);
-    own_string(input);
-
-    // println!("{}", input);
-    // let mut mars_weight = calculate_weight_on_mars(100.0);
-    // println!("Weight on Mars: {}kg", mars_weight);
-}
-
-fn borrow_string(s: &String) {
-    println!("{}", s);
-}
-
-fn own_string(s: String) {
-    println!("{}", s);
+    io::stdin().read_line(&mut input).unwrap();
+    let weight: f32 = input.trim().parse().unwrap();
+    let mars_weight = calculate_weight_on_mars(weight);
+    println!("Weight on Mars: {}kg", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
-
-// 1. Each value in Rust is owned by a variable.
-
-// 2. When the owner goes out of scope. the value will be deallocated.
-
-// 3. There can be only ONE owner at a time.
