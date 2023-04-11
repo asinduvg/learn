c868a48c03f5c1bc50bb79335d3d220101f050a7
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-11 15:40:18 +0530

    Complete Structs and Strings

---
+++ rust/tutorials/learn-rust-by-building-real-applications/server/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    // let string = String::from("127.0.0.1:8080");
+    // let string_slice = &string[10..14];
+    // let string_borrow: &str = &string;
+    // let string_literal = "1234";
+
+    // dbg!(&string);
+    // dbg!(string_slice);
+    // dbg!(string_borrow);
+    // dbg!(string_literal);
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
