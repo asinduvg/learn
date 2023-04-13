3072bf60bc110854074b94da4225d6f7ee32148c
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-04-13 17:42:17 +0530

    #rust,rust1: Complete Advanced Error Handling

--- rust/tutorials/learn-rust-by-building-real-applications/server/src/http/request.rs
+++ rust/tutorials/learn-rust-by-building-real-applications/server/src/http/request.rs
@@ -1,6 +1,8 @@
+use std::str::Utf8Error;
 use super::method::Method;
 use std::error::Error;
 use std::fmt::{Display, Debug, Formatter, Result as FmtResult};
+use std::str;
 
 pub struct Request {
     path: String,
@@ -11,7 +13,21 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        match str::from_utf8(buf) {
+            Ok(request) => {},
+            Err(_) => return Err(ParseError::InvalidEncoding)
+        }
+
+        match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+            Ok(request) => {},
+            Err(e) => return Err(e)
+        }
+
+        let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?;
+
+        let requestAlt = str::from_utf8(buf)?;
+
         unimplemented!()
     }
 }
@@ -31,6 +47,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method"
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
