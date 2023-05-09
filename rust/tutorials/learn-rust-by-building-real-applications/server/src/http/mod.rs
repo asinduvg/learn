8b0a947a87c5ece43a76c99b83bb6c7f33d9fcef
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-05-09 08:18:52 +0530

    #rust,rust1: Complete Modelling the HTTP Response

--- rust/tutorials/learn-rust-by-building-real-applications/server/src/http/mod.rs
+++ rust/tutorials/learn-rust-by-building-real-applications/server/src/http/mod.rs
@@ -1,7 +1,9 @@
-pub use request::{Request, ParseError};
 pub use method::Method;
 pub use query_string::{QueryString, Value as QueryStringValue};
+pub use request::{Request, ParseError};
+pub use response::Response;
 
 pub mod request;
+pub mod response;
 pub mod method;
 pub mod query_string;
