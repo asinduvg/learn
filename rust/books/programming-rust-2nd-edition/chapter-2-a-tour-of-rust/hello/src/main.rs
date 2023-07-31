f84f46c40c5eb7ce74199e05e3498768b33975ea
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2023-07-31 03:14:02 +0000

    #rust,rust2: Complete Handling Command Line Arguments

--- rust/books/programming-rust-2nd-edition/chapter-2-a-tour-of-rust/hello/src/main.rs
+++ rust/books/programming-rust-2nd-edition/chapter-2-a-tour-of-rust/hello/src/main.rs
@@ -1,14 +1,27 @@
+use std::str::FromStr;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
-    println!("{}", gcd(60, 87));
+    let mut numbers = Vec::new();
 
-    let _s = {
-        let x: i32 = 180;
-        println!("Calculating 183..");
-        x + 3
-    };
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+        .expect("error parsing argument"));
+    }
 
-    print!("{}", _s);
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}",
+                numbers, d);
+
 }
 
 // Rust Functions
